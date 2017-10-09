be94d19c506f67cfa2677ccd858c7e3c0142c3c0
Oscar <o.d.periferia@gmail.com>
2017-10-09 15:39:43 -0500

    prueba de commit

--- Hola mundo/ViewController.swift
+++ Hola mundo/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ChangeMessage(sender: AnyObject) {
+        messageLabel.text = "\(nameTextField.text)"
+        view.endEditing(true)
+    }
+    
+    // ejemplo de commit
 
 }
 
